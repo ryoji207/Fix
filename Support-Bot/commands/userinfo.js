bad3cf476ad1443b1735bef6ee0f5c633851a571
SenforSt <hoangnhan26212@gmail.com>
2020-03-26 18:03:17 +0700

    Update userinfo.js
    hmmm

--- Support-Bot/commands/userinfo.js
+++ Support-Bot/commands/userinfo.js
@@ -7,18 +7,28 @@ const colours = require("../colours.json");
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || message.author;
 
+    let infoUser = {
+        id:user.id,
+        avatar:user.displayAvatarURL(),
+        name:user.username,
+        tag:user.discriminator,
+        status:user.presence.status,
+        joined:user.joinedAt
+    }
+    console.log(user.displayAvatarURL());
     let uEmbed = new Discord.MessageEmbed()
-    .setColor(colours.cyan)
-    .setTitle("Thông tin người dùng")
-    .setThumbnail(user.displayAvatarURL)
-    .setAuthor(message.guild.name, message.guild.iconURL())
-    .setDescription("ID:", user.id)
-    .addField("Tên:", user.username, true)
-    .addField("Mã số:", user.discriminator, true)
-    .addField("ID:", user.id, true)
-    .addField("Thông tin:", user.presence.status, true)
-    .addField("Ngày tham gia:", user.joinedAt, true)
-    .setFooter(bot.user.displayAvatarURL);
+        .setColor(colours.cyan)
+        .setTitle("Thông tin người dùng")
+        .setAuthor(message.guild.name,infoUser.avatar)
+        .setThumbnail(infoUser.avatar)
+        .setDescription("ID:\n"+ infoUser.id)
+        .addField("Tên:", infoUser.name, true)
+        .addField("Mã số:", infoUser.tag, true)
+        .addField("ID:", infoUser.id, true)
+        .addField("Thông tin:", infoUser.status, true)
+        .addField("Ngày tham gia:", infoUser.joiend, true)
+   // .setFooter(bot.user.displayAvatarURL);
+     return message.channel.send(uEmbed);
 
 }
 
